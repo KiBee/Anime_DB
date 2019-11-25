dd17ac304237910f3d359647f4a69354dcf6b1ac
KiBee <killerbee1205@gmail.com>
2019-11-25 22:08:06 +1000

    Merge remote-tracking branch 'origin/testing' into testing
    # Conflicts:
    #	pr_lic_st_gen_anime.py

--- pr_lic_st_gen_anime.py
+++ pr_lic_st_gen_anime.py
@@ -7,17 +7,13 @@ import sqlalchemy
 #           animelist_producer, animelist_licensor, animelist_studio, animelist_genres
 #  and create .csv files to */csv
 
+
 st_csv_filename = 'csv\\initial csv\\anime_filtered.csv'
 
 mysql_engine = 'mysql://root@localhost:3306/anime_norm_maria?charset=utf8'
 engine = sqlalchemy.create_engine(mysql_engine)
 
-# q = """
-#         SELECT *
-#         FROM anime_filtered_full
-#     """
-
-plsg = pd.read_csv(st_csv_filename)
+anime = pd.read_csv(st_csv_filename)
 
 # main_files
 prods_file = 'csv\\producer.csv'
@@ -61,7 +57,9 @@ staging_lists = [prods_animelist_list, licens_animelist_list, studs_animelist_li
 staging_files = [prods_animelist_file, licens_animelist_file, studs_animelist_file, genres_animelist_file]
 
 
-# перезапись поля duration
+
+
+# перезапись поля duration (перевод в минуты)
 def change_dur(x):
     if 'per ep' in x:
         x = x[:-8]
@@ -82,6 +80,7 @@ def change_dur(x):
     return x
 
 
+# перезапись полей с url_image(битые ссылки)
 def change_url_img(x):
     if type(x) != float:
         x = str(x.replace('https://myanimelist.cdn-dena.com/', 'https://cdn.myanimelist.net/'))
@@ -89,84 +88,97 @@ def change_url_img(x):
 
 
 # смещение индекса для корректной записи в таблицы и csv
-# def reindexing(lst):
-#     rdf = pd.DataFrame(lst)
-#     rdf.index = rdf.index + 1
-#     return rdf
-#
-#
-# # запись основных таблиц в csv
-# for s, c, l, f in zip(sets, cols, main_lists, main_files):
-#     for k, v in plsg.iterrows():
-#         if not v[c] is None and str(v[c]) != 'nan':
-#             v[c] = v[c].replace('&#039;', "'").replace('&amp;', "&")
-#             s.update(set(list(map(str, v[c].split(', ')))))
-#     l.append('None')
-#     l.extend(sorted(s))
-#     reindexing(l).to_csv(f, header=None, encoding='utf-8-sig')
-#     print(f, 'updated')
-#
-# # запись основных таблиц в базу
-# print()
-# reindexing(prods_list).rename(columns={0: 'title_producer'}).to_sql('producer', index='id_producer',  if_exists='append', con=engine)
-# print('Table Producer updated')
-# reindexing(licens_list).rename(columns={0: 'title_licensor'}).to_sql('licensor', index='id_licensor',  if_exists='append', con=engine)
-# print('Table Licensor updated')
-# reindexing(studs_list).rename(columns={0: 'title_studio'}).to_sql('studio', index='id_studio',  if_exists='append', con=engine)
-# print('Table Studio updated')
-# reindexing(genres_list).rename(columns={0: 'title_genre'}).to_sql('genres', index='id_genre',  if_exists='append', con=engine)
-# print('Table Genres updated')
-#
-# # запись промежуточных таблиц в csv
-# for c, m, stl, stf in zip(cols, main_lists, staging_lists, staging_files):
-#     for k, v in plsg.iterrows():
-#         if not v[c] is None and str(v[c]) != 'nan':
-#             v[c] = v[c].replace('&#039;', "'").replace('&amp;', "&")
-#             buf_list = list(map(str, v[c].split(', ')))
-#             for i in range(len(buf_list)):
-#                 stl.append(list(map(int, (v.anime_id, m.index(buf_list[i])))))
-#         else:
-#             stl.append(list(map(int, (v.anime_id, 1))))
-#     reindexing(stl).to_csv(stf, header=None, index=None, encoding='utf-8-sig')
-#     print(stf, 'updated')
-#
-# # запись промежуточных таблиц в базу
-# reindexing(prods_animelist_list).rename(columns={0: 'id_anime', 1: 'id_producer'}).to_sql('anime_producer', index=False,  if_exists='append', con=engine)
-# print('Table Animelist_producer updated')
-# reindexing(licens_animelist_list).rename(columns={0: 'id_anime', 1: 'id_licensor'}).to_sql('anime_licensor', index=False,  if_exists='append', con=engine)
-# print('Table Animelist_licensor updated')
-# reindexing(studs_animelist_list).rename(columns={0: 'id_anime', 1: 'id_studio'}).to_sql('anime_studio', index=False,  if_exists='append', con=engine)
-# print('Table Animelist_studio updated')
-# reindexing(genres_animelist_list).rename(columns={0: 'id_anime', 1: 'id_genre'}).to_sql('anime_genre', index=False,  if_exists='append', con=engine)
-# print('Table Animelist_genre updated')
-# print('Completed!')
+def reindexing(lst):
+    rdf = pd.DataFrame(lst)
+    rdf.index = rdf.index + 1
+    return rdf
 
-plsg['url_mal'] = 'https://myanimelist.net/anime/' + plsg.anime_id.apply(str)
-plsg['url_shiki'] = 'https://shikimori.one/animes/' + plsg.anime_id.apply(str)
 
-plsg.duration = plsg.duration.apply(change_dur)
+for i in cols:
+    q = f"""
+        DELETE
+        FROM {i}
+    """
+    cursor = engine.connect()
+    cursor.execute(q)
 
-plsg.image_url = plsg.image_url.apply(change_url_img)
+# добавление  полей с сылками на myanimelist и shikimori
+anime['url_mal'] = 'https://myanimelist.net/anime/' + anime.anime_id.apply(str)
+anime['url_shiki'] = 'https://shikimori.one/animes/' + anime.anime_id.apply(str)
 
-plsg.drop(
+# перезапись поля duration (перевод в минуты)
+anime.duration = anime.duration.apply(change_dur)
+
+# перезапись полей с url_image(битые ссылки)
+anime.image_url = anime.image_url.apply(change_url_img)
+
+# запись основных таблиц в csv
+for s, c, l, f in zip(sets, cols, main_lists, main_files):
+    for k, v in anime.iterrows():
+        if not v[c] is None and str(v[c]) != 'nan':
+            v[c] = v[c].replace('&#039;', "'").replace('&amp;', "&")
+            s.update(set(list(map(str, v[c].split(', ')))))
+    l.append('None')
+    l.extend(sorted(s))
+    reindexing(l).to_csv(f, header=None, encoding='utf-8-sig')
+    print(f, 'updated')
+
+
+# запись основных таблиц в базу
+print()
+reindexing(prods_list).rename(columns={0: 'title_producer'}).to_sql('producer', index='id_producer',  if_exists='append', con=engine)
+print('Table Producer updated')
+reindexing(licens_list).rename(columns={0: 'title_licensor'}).to_sql('licensor', index='id_licensor',  if_exists='append', con=engine)
+print('Table Licensor updated')
+reindexing(studs_list).rename(columns={0: 'title_studio'}).to_sql('studio', index='id_studio',  if_exists='append', con=engine)
+print('Table Studio updated')
+reindexing(genres_list).rename(columns={0: 'title_genre'}).to_sql('genre', index='id_genre',  if_exists='append', con=engine)
+print('Table Genres updated')
+
+# запись промежуточных таблиц в csv
+for c, m, stl, stf in zip(cols, main_lists, staging_lists, staging_files):
+    for k, v in anime.iterrows():
+        if not v[c] is None and str(v[c]) != 'nan':
+            v[c] = v[c].replace('&#039;', "'").replace('&amp;', "&")
+            buf_list = list(map(str, v[c].split(', ')))
+            for i in range(len(buf_list)):
+                stl.append(list(map(int, (v.anime_id, m.index(buf_list[i])))))
+        else:
+            stl.append(list(map(int, (v.anime_id, 1))))
+    reindexing(stl).to_csv(stf, header=None, index=None, encoding='utf-8-sig')
+    print(stf, 'updated')
+
+# запись промежуточных таблиц в базу
+reindexing(prods_animelist_list).rename(columns={0: 'id_anime', 1: 'id_producer'}).to_sql('anime_producer', index=False,  if_exists='replace', con=engine)
+print('Table Animelist_producer updated')
+reindexing(licens_animelist_list).rename(columns={0: 'id_anime', 1: 'id_licensor'}).to_sql('anime_licensor', index=False,  if_exists='replace', con=engine)
+print('Table Animelist_licensor updated')
+reindexing(studs_animelist_list).rename(columns={0: 'id_anime', 1: 'id_studio'}).to_sql('anime_studio', index=False,  if_exists='replace', con=engine)
+print('Table Animelist_studio updated')
+reindexing(genres_animelist_list).rename(columns={0: 'id_anime', 1: 'id_genre'}).to_sql('anime_genre', index=False,  if_exists='replace', con=engine)
+print('Table Animelist_genre updated')
+print('Completed!')
+
+
+anime.drop(
     columns=['airing', 'aired_string', 'aired', 'score', 'scored_by', 'rank', 'popularity', 'members', 'favorites',
              'background', 'premiered', 'broadcast', 'related', 'producer', 'licensor', 'studio', 'genre'],
     inplace=True)
 
-plsg.rename(columns={0: 'anime_id',
-                     1: 'title',
-                     2: 'title_english',
-                     3: 'title_japanese',
-                     4: 'title_synonyms',
-                     5: 'image_url',
-                     6: 'type',
-                     7: 'source',
-                     8: 'episodes',
-                     9: 'status',
-                     13: 'duration',
-                     14: 'rating',
-                     20: 'opening_theme',
-                     21: 'ending_theme',
-                     22: 'url_mal',
-                     23: 'url_shiki'
-                     }).to_sql('test_anime', index=False, if_exists='replace', con=engine)
+anime.rename(columns={0: 'anime_id',
+                      1: 'title',
+                      2: 'title_english',
+                      3: 'title_japanese',
+                      4: 'title_synonyms',
+                      5: 'image_url',
+                      6: 'type',
+                      7: 'source',
+                      8: 'episodes',
+                      9: 'status',
+                      13: 'duration',
+                      14: 'rating',
+                      19: 'opening_theme',
+                      20: 'ending_theme',
+                      21: 'url_mal',
+                      22: 'url_shiki'
+                      }).to_sql('test_anime', index=False, if_exists='replace', con=engine)
