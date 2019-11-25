024c558cd1ceab9d0d8bf2eb0f26b7b3b6bb9d66
KiBee <killerbee1205@gmail.com>
2019-11-25 17:23:53 +1000

    upd

--- duration_time.py
+++ duration_time.py
@@ -2,41 +2,67 @@ import pandas as pd
 import sqlalchemy
 
 mysql_engine = 'mysql://root@localhost:3306/anime_norm_maria?charset=utf8'
-
 engine = sqlalchemy.create_engine(mysql_engine)
 
+st_csv_filename = 'csv\\initial csv\\anime_filtered.csv'
 
-q = """
-    SELECT *
-    from test_anime
-    """
+
+in_df = pd.read_csv(st_csv_filename)
+
+in_df['url_mal'] = 'https://myanimelist.net/anime/' + in_df.anime_id.apply(str)
+in_df['url_shiki'] = 'https://shikimori.one/animes/' + in_df.anime_id.apply(str)
+
 
 dur = list()
 id = list()
-a = pd.read_sql(q, engine)
 
-for k, v in a.iterrows():
-    dur.append(v.duration)
-    id.append(v.anime_id)
-    if 'per ep' in dur[k]:
-        dur[k] = dur[k][:-8]
+# for k, v in in_df.iterrows():
+#     dur.append(v.duration)
+#     id.append(v.anime_id)
+#     if 'per ep' in dur[k]:
+#         dur[k] = dur[k][:-8]
+#
+#     if 'min' in dur[k]:
+#         dur[k] = dur[k][:-5]
+#
+#     if 'hr' in dur[k] and len(dur[k]) > 5:
+#         dur[k] = int(60 * int(dur[k][0])) + int(dur[k][6:])
+#     elif 'hr' in dur[k]:
+#         dur[k] = int(60 * int(dur[k][0]))
+#
+#     if type(dur[k]) != int and 'sec' in dur[k]:
+#         dur[k] = '1'
+#     if type(dur[k]) != int and 'Unknown' in dur[k]:
+#         dur[k] = 0
 
-    if 'min' in dur[k]:
-        dur[k] = dur[k][:-5]
 
-    if 'hr' in dur[k] and len(dur[k]) > 5:
-        dur[k] = int(60 * int(dur[k][0])) + int(dur[k][6:])
-    elif 'hr' in dur[k]:
-        dur[k] = int(60 * int(dur[k][0]))
+def change_dur(x):
 
-    if type(dur[k]) != int and 'sec' in dur[k]:
-        dur[k] = '1'
-    if type(dur[k]) != int and 'Unknown' in dur[k]:
-        dur[k] = 0
+    # dur.append(v.duration)
+    # id.append(v.anime_id)
+
+    if 'per ep' in x:
+        x = x[:-8]
+
+    if 'min' in x:
+        x = x[:-5]
+
+    if 'hr' in x and len(x) > 5:
+        x = int(60 * int(x[0])) + int(x[6:])
+    elif 'hr' in x:
+        x = int(60 * int(x[0]))
+
+    if type(x) != int and 'sec' in x:
+        x = '1'
+    if type(x) != int and 'Unknown' in x:
+        x = 0
+    return x
 
 
-for i in range(len(id)):
-    a.loc[a['anime_id'] == id[i], 'duration'] = dur[i]
+in_df.duration.apply(change_dur)
+
+# for i in range(len(id)):
+#     in_df.loc[in_df['anime_id'] == id[i], 'duration'] = dur[i]
 
 
 
