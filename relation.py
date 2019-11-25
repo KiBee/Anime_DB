3c4d94b328c968bd2acdb8ea2aa1a2b3348b3a1f
KiBee <killerbee1205@gmail.com>
2019-11-25 21:32:27 +1000

    upd

---
+++ relation.py
@@ -0,0 +1,84 @@
+import os
+import pandas as pd
+import sqlalchemy
+
+os.makedirs('csv', exist_ok=True)
+os.makedirs('csv\\initial csv', exist_ok=True)
+os.makedirs('csv\\test csv', exist_ok=True)
+
+
+st_csv_filename = 'csv\\initial csv\\anime_filtered.csv'
+
+filename = 'csv\\relation.csv'
+
+mysql_engine = 'mysql://root@localhost:3306/anime_norm_maria?charset=utf8'
+engine = sqlalchemy.create_engine(mysql_engine)
+
+initial_df = pd.read_csv(st_csv_filename)
+obj_id = list()
+full_filtered_list = list()
+table = list()
+
+for k, v in initial_df.iterrows():
+    # obj_id.append(v.anime_id)
+    # v.image_url.replace('https://myanimelist.cdn-dena.com/', 'https://cdn.myanimelist.net/')
+
+
+
+    if v.related != "[]":
+        w = list()
+        c = list()
+        filtered_list = list()
+        a = list(map(str, v.related.replace("'", "").split('}]')))
+
+        for i in range(len(a)):
+            a[i] = a[i].replace(", ", " ")
+            c.append(a[i].split(": [{"))
+
+        for i in range(len(c)):
+
+            c[i][0] = c[i][0].replace('{', "").replace('}', "")
+            if c[i][0] == "":
+                c.pop(i)
+                i -= 1
+
+            if c[i][0][0] == " ":
+                c[i][0] = c[i][0][:0] + c[i][0][1:]
+            c[i][0].split('}{')
+
+        for i in range(len(c)):
+            w.append(c[i])
+            w[i][1] = w[i][1].split("} {")
+            if w[i][1][0].find("type: anime") != -1:
+                for j in range(len(w[i][1])):
+                    w[i][1][j] = w[i][1][j].split(" ")[1]
+                filtered_list.append(w[i])
+
+        full_filtered_list.append(filtered_list)
+
+    else:
+        full_filtered_list.append('empty')
+
+for k in range(len(obj_id)):
+    try:
+        for i in range(len(full_filtered_list[k])):
+            for j in range(len(full_filtered_list[k][i][1])):
+                if int(full_filtered_list[k][i][1][j]) != 0:
+                    table.append((int(obj_id[k]), full_filtered_list[k][i][0], int(full_filtered_list[k][i][1][j])))
+    except IndexError:
+        pass
+sch = 0
+for i in range(len(table) - 1, 0, -1):
+    if table[i][2] not in obj_id or table[i][2] == table[i][0]:
+        sch += 1
+        print(sch)
+        table.pop(i)
+
+print(sch)
+
+
+# zp = pd.DataFrame(sorted(table, key=lambda x: (x[0], x[1], x[2])))
+# zp.to_csv(filename, header=None, index=False, encoding='utf-8-sig')
+# print(filename, 'updated!')
+# zp.rename(columns={0: 'id_anime_object', 1: 'id_relation', 2: 'id_anime_subject'}).to_sql('relation', index=False,  if_exists='append', con=engine)
+# print('Table Relation updated!')
