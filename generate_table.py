751e120a7bb922f3bcb137cef27cb24849fdc13b
KiBee <killerbee1205@gmail.com>
2019-12-14 20:51:06 +1000

    refactoring generate

--- generate_table.py
+++ generate_table.py
@@ -26,26 +26,6 @@ licens_table = 'licensor'
 studs_table = 'studio'
 genres_table = 'genre'
 
-# main_files (ссылки на файлы основных таблиц)
-prods_file = 'csv\\' + prods_table + '.csv'
-licens_file = 'csv\\' + licens_table + '.csv'
-studs_file = 'csv\\' + studs_table + '.csv'
-genres_file = 'csv\\' + genres_table + '.csv'
-relation_file = 'csv\\' + relation_table + '.csv'
-anime_file = 'csv\\' + anime_table + '.csv'
-
-# staging_files (ссылки на файлы промежуточных таблиц)
-prods_anime_file = 'csv\\anime_' + prods_table + '.csv'
-licens_anime_file = 'csv\\anime_' + licens_table + '.csv'
-studs_anime_file = 'csv\\anime_' + studs_table + '.csv'
-genres_anime_file = 'csv\\anime_' + genres_table + '.csv'
-
-# sets (множества для отбора уникальных значений для заполнения основных таблиц)
-prods = set()
-licens = set()
-studs = set()
-genres = set()
-
 # main_lists (списки для основных таблиц)
 prods_list = list()
 licens_list = list()
@@ -64,25 +44,29 @@ relation = list()
 cols = [prods_table, licens_table, studs_table, genres_table]
 
 # список множеств для отбора уникальных значений для заполнения основных таблиц
-sets = [prods, licens, studs, genres]
+sets = [set(), set(), set(), set()]
 
 # списки для основных таблиц (названия и файлы)
 main_lists = [prods_list, licens_list, studs_list, genres_list]
-main_files = [prods_file, licens_file, studs_file, genres_file]
 
 # списки для промежуточных таблиц (названия и файлы)
 staging_lists = [prods_anime_list, licens_anime_list, studs_anime_list, genres_anime_list]
-staging_files = [prods_anime_file, licens_anime_file, studs_anime_file, genres_anime_file]
+
+
+def get_stf_name(t):
+    return 'csv\\anime_' + t + '.csv'
+
+
+def get_mf_name(t):
+    return 'csv\\' + t + '.csv'
 
 
 # перезапись поля duration (перевод в минуты)
 def change_dur(x):
     if 'per ep' in x:
         x = x[:-8]
-
     if 'min' in x:
         x = x[:-5]
-
     if 'hr' in x and len(x) > 5:
         x = int(60 * int(x[0])) + int(x[6:])
     elif 'hr' in x:
@@ -132,7 +116,9 @@ for ind, data in anime.iterrows():
                 if subid in anime_id_list:
                     if subid != data.anime_id:
                         relation.append((data.anime_id, i, subid))
-relation = sorted(relation, key=lambda x: (x[0], x[1], x[2]))
+
+relation = pd.DataFrame(sorted(relation, key=lambda x: (x[0], x[1], x[2]))).rename(
+    columns={0: 'id_anime_object', 1: 'title_relation', 2: 'id_anime_subject'})
 
 # добавление  полей с сылками на myanimelist и shikimori
 anime['url_mal'] = 'https://myanimelist.net/anime/' + anime.anime_id.apply(str)
@@ -142,8 +128,7 @@ anime['url_shiki'] = 'https://shikimori.one/animes/' + anime.anime_id.apply(str)
 anime['airing'] = anime.airing.apply(bool)
 
 # замена амперсандов и апострофов во всей таблице
-anime = anime.replace(
-    to_replace=r'&#039;', value="'", regex=True).replace(to_replace=r'&amp;', value="&", regex=True)
+anime = anime.replace(to_replace={0: r'&#039;', 1: r'&amp;'}, value={0: "'", 1: "&"}, regex=True)
 
 # замена ссылок на картинки на корректные
 anime.image_url = anime.image_url.replace(
@@ -158,27 +143,26 @@ anime = anime.join(anime.broadcast.str.replace(r' .JST.', '').str.split(' at ', 
     columns={0: 'broadcast_day', 1: 'broadcast_time'})
 
 # разделение aired(даты показа) на start date и finish date
-anime = anime.join(
-    anime.aired_string.str.split(' to ', expand=True)).rename(columns={0: 'start_date', 1: 'finish_date'})
+anime = anime.join(anime.aired_string.str.split(' to ', expand=True)).rename(
+    columns={0: 'start_date', 1: 'finish_date'})
 
 # перезапись поля duration (перевод в минуты)
 anime.duration = anime.duration.apply(change_dur)
 
-# Запись главной таблицы anime в csv
-anime.drop(
+buf = anime.drop(
     columns=['aired_string', 'aired', 'score', 'scored_by', 'rank', 'popularity', 'members', 'favorites',
-             'background', 'premiered', 'broadcast', 'related', 'producer', 'licensor', 'studio', 'genre']).to_csv(
-    anime_file, index=None, encoding='utf-8-sig')
-print('File', anime_file, 'updated!')
+             'background', 'premiered', 'broadcast', 'related', 'producer', 'licensor', 'studio', 'genre'])
+
+# Запись главной таблицы anime в csv
+buf.to_csv(get_mf_name(anime_table), index=None, encoding='utf-8-sig')
+print('File', get_mf_name(anime_table), 'updated!')
 
 # Запись главной таблицы anime в базу
-anime.drop(
-    columns=['aired_string', 'aired', 'score', 'scored_by', 'rank', 'popularity', 'members', 'favorites',
-             'background', 'premiered', 'broadcast', 'related', 'producer', 'licensor', 'studio', 'genre']).to_sql(
-    anime_table, index=False, if_exists='append', con=engine)
+buf.to_sql(anime_table, index=False, if_exists='append', con=engine)
 print('Table', anime_table, 'updated!')
+print()
 
-for s, col, mlist, mfile, slist, sfile in zip(sets, cols, main_lists, main_files, staging_lists, staging_files):
+for s, col, mlist, slist in zip(sets, cols, main_lists, staging_lists):
     # образование списков для основных таблиц
     for ind, data in anime.iterrows():
         if not data[col] is None and str(data[col]) != 'nan':
@@ -187,8 +171,8 @@ for s, col, mlist, mfile, slist, sfile in zip(sets, cols, main_lists, main_files
     mlist.extend(sorted(s))
 
     # запись основных таблиц в csv
-    reindexing(mlist).to_csv(mfile, header=None, encoding='utf-8-sig')
-    print('File', mfile, 'updated!')
+    reindexing(mlist).to_csv(get_mf_name(col), header=None, encoding='utf-8-sig')
+    print('File', get_mf_name(col), 'updated!')
 
     # запись основных таблиц в базу
     reindexing(mlist).rename(columns={0: f'title_{col}'}).to_sql(col, index=f'id_{col}', if_exists='append', con=engine)
@@ -204,8 +188,8 @@ for s, col, mlist, mfile, slist, sfile in zip(sets, cols, main_lists, main_files
             slist.append(list(map(int, (data.anime_id, 1))))
 
     # запись промежуточных таблиц в csv
-    pd.DataFrame(slist).to_csv(sfile, header=None, index=None, encoding='utf-8-sig')
-    print('File', sfile, 'updated!')
+    pd.DataFrame(slist).to_csv(get_stf_name(col), header=None, index=None, encoding='utf-8-sig')
+    print('File', get_stf_name(col), 'updated!')
 
     # запись промежуточных таблиц в базу
     pd.DataFrame(slist).rename(
@@ -214,17 +198,10 @@ for s, col, mlist, mfile, slist, sfile in zip(sets, cols, main_lists, main_files
     print()
 
 # Запись таблицы relation (связи между аниме)
-pd.DataFrame(relation).replace(
-    to_replace=r'&#039;', value="'", regex=True).replace(
-    to_replace=r'&amp;', value="&", regex=True).rename(
-    columns={0: 'id_anime_object', 1: 'title_relation', 2: 'id_anime_subject'}).to_sql(
-    relation_table, index=False, if_exists='append', con=engine)
+relation.to_sql(relation_table, index=False, if_exists='append', con=engine)
 print('Table', relation_table, 'updated!')
 
 # Запись файла relation.csv (связи между аниме)
-pd.DataFrame(relation).replace(
-    to_replace=r'&#039;', value="'", regex=True).replace(
-    to_replace=r'&amp;', value="&", regex=True).to_csv(
-    relation_file, header=None, index=False, encoding='utf-8-sig')
-print('File', relation_file, 'updated!')
+relation.to_csv(get_mf_name(relation_table), header=None, index=False, encoding='utf-8-sig')
+print('File', get_mf_name(relation_table), 'updated!')
 print('Complete!')
